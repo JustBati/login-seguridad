8f0fe9cfc07bb6f8e34444305e28583d8b4b01ef
Acidjeremy07 <jcazcru@gmail.com>
2024-01-18 17:34:49 -0600

    Creación de requerimientos
    Mediante pip freeze se crea el documento de las dependencias necesarias para el programa

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qer6b*#*dew4bt*)xkb51j$cluku3ojcg33)+@9%#0bjzm=qp0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True    
 
 ALLOWED_HOSTS = ['*']
 
